f47a19ee4fc24cca44734b4f00e6d8b64b14be00
kluchev <kluchev@gmail.com>
2023-03-26 13:43:47 +0300

    В сборку добавлна dokka, обновлены библиотеки

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.dokka") version "1.7.10"
     application
 }
 
-group = "org.example"
+group = "ru.itmo"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -15,12 +16,15 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 }
 
 tasks.test {
     useJUnit()
+
+    filter {
+        includeTestsMatching("*Test*")
+    }
 }
 
 tasks.withType<KotlinCompile> {
@@ -29,4 +33,12 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.dokkaHtml.configure {
+    outputDirectory.set(buildDir.resolve("dokka-html"))
+}
+
+tasks.dokkaGfm.configure {
+    outputDirectory.set(buildDir.resolve("dokka"))
 }
